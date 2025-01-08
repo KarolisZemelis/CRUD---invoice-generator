1e48c81beadab249c5ebc3e42fd3eb6c5ca753f3
KarolisZemelis <karoliszemlis@gmail.com>
2025-01-08 13:39:34 +0200

    forgot to add priceAfterDiscount once there is an update = fixed

--- public/invoiceList.js
+++ public/invoiceList.js
@@ -3,26 +3,37 @@
   !*** ./src/invoiceList.js ***!
   \****************************/
 tableRow = document.querySelectorAll("[data-table-items]");
+itemTotal = 0;
+function calculateTotals(itemTotal) {}
 document.addEventListener("DOMContentLoaded", function () {
+  var rowSum = 0;
   tableRow.forEach(function (row) {
+    rowSum = row.querySelector("[data-table-rowTotal]").innerText;
+    console.log(rowSum);
     row.addEventListener("input", function () {
       var qtyInput = parseFloat(row.querySelector("[data-input-qty] input").value);
       var price = parseFloat(row.querySelector("[data-table-item-price]").innerText);
       var discountPercentage = row.querySelector("[data-input-percentage-value]");
       var discountFixed = row.querySelector("[data-input-fixed-value]");
+      var priceAfterDiscountDOM = row.querySelector("[data-table-priceAfterDiscount]");
       var priceAfterDiscount = 0;
       if (discountPercentage) {
         discountPercentage = parseFloat(discountPercentage.value) / 100;
         priceAfterDiscount = parseFloat(price - price * discountPercentage);
+        priceAfterDiscountDOM.innerText = priceAfterDiscount;
       } else if (discountFixed) {
         discountFixed = parseFloat(Math.abs(discountFixed.value));
         priceAfterDiscount = parseFloat((price - discountFixed).toFixed(2));
+        priceAfterDiscountDOM.innerText = priceAfterDiscount;
       }
-      var rowSum = 0;
+      rowSum = 0;
       priceAfterDiscount > 0 ? rowSum = parseFloat(qtyInput * priceAfterDiscount).toFixed(2) : rowSum = parseFloat(qtyInput * price).toFixed(2);
       row.querySelector("[data-table-rowTotal]").innerText = rowSum;
+      console.log(itemTotal);
     });
+    itemTotal += parseFloat(rowSum);
   });
+  console.log(itemTotal);
 });
 /******/ })()
 ;
--- src/invoiceList.js
+++ src/invoiceList.js
@@ -1,7 +1,12 @@
 tableRow = document.querySelectorAll("[data-table-items]");
+itemTotal = 0;
+function calculateTotals(itemTotal) {}
 
 document.addEventListener("DOMContentLoaded", () => {
+  let rowSum = 0;
   tableRow.forEach((row) => {
+    rowSum = row.querySelector("[data-table-rowTotal]").innerText;
+    console.log(rowSum);
     row.addEventListener("input", () => {
       let qtyInput = parseFloat(
         row.querySelector("[data-input-qty] input").value
@@ -13,22 +18,31 @@ document.addEventListener("DOMContentLoaded", () => {
         "[data-input-percentage-value]"
       );
       let discountFixed = row.querySelector("[data-input-fixed-value]");
+      let priceAfterDiscountDOM = row.querySelector(
+        "[data-table-priceAfterDiscount]"
+      );
       let priceAfterDiscount = 0;
       if (discountPercentage) {
         discountPercentage = parseFloat(discountPercentage.value) / 100;
         priceAfterDiscount = parseFloat(price - price * discountPercentage);
+        priceAfterDiscountDOM.innerText = priceAfterDiscount;
       } else if (discountFixed) {
         discountFixed = parseFloat(Math.abs(discountFixed.value));
         priceAfterDiscount = parseFloat((price - discountFixed).toFixed(2));
+        priceAfterDiscountDOM.innerText = priceAfterDiscount;
       }
 
-      let rowSum = 0;
+      rowSum = 0;
 
       priceAfterDiscount > 0
         ? (rowSum = parseFloat(qtyInput * priceAfterDiscount).toFixed(2))
         : (rowSum = parseFloat(qtyInput * price).toFixed(2));
 
       row.querySelector("[data-table-rowTotal]").innerText = rowSum;
+      console.log(itemTotal);
     });
+    itemTotal += parseFloat(rowSum);
   });
+
+  console.log(itemTotal);
 });
